d5fb2f64b38779c04d4428320aef0765f13e7bff
Frederick Shier <fred.shier@gmail.com>
2022-04-10 07:35:28 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 go build -a -o ./bin/neb-mc-wrapper
 
-tar -czvf ./build/neb-mc-wrapper.tgz ./bin/neb-mc-wrapper --remove-files
+tar -czvf ./build/neb-mc-wrapper.tgz -C ./bin neb-mc-wrapper --remove-files
